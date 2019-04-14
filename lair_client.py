00e5a234e94d57157dd0b6fc001a9d3583e31166
Michael Berry <trismegustis@gmail.com>
2019-04-14 10:48:37 -0400

    Fixed unused variable warnings.

--- lair_client.py
+++ lair_client.py
@@ -58,7 +58,7 @@ class ChatClient():
         """Select between reading from server socket and standard input."""
         sockets = [sys.stdin, self.server]
 
-        rlist, wlist, xlist = select.select(sockets, [], [], 60)
+        rlist, (), () = select.select(sockets, [], [], 60)
 
         for sock in rlist:
             if sock == self.server:
