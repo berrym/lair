a47a57a025817b768827096cf1f680a3442fa05a
Michael Berry <trismegustis@gmail.com>
2019-07-02 09:49:31 -0400

    Renamed global variables.

--- lair_client-qt.py
+++ lair_client-qt.py
@@ -7,10 +7,10 @@ import psutil
 from threading import Thread
 from PyQt5 import QtCore, QtWidgets
 
-addr = '127.0.0.1'
-port = 1234
-tcp_client = None
-exit_flag = False
+ADDR = '127.0.0.1'
+PORT = 1234
+TCP_CLIENT = None
+EXIT_FLAG = False
 
 
 def kill_proc_tree(pid):
@@ -24,7 +24,7 @@ class ConnectionDialog(QtWidgets.QDialog):
     def __init__(self):
         """Create a simple dialog.
 
-        Populate addr and port of server with user input.
+        Populate ADDR and port of server with user input.
         """
         super().__init__()
         self.addressLabel = QtWidgets.QLabel('Server Address', self)
@@ -41,14 +41,14 @@ class ConnectionDialog(QtWidgets.QDialog):
         self.vbox.addWidget(self.btnConnect)
         self.setLayout(self.vbox)
         self.setWindowTitle('Connect to Lair Server')
-        self.resize(250, 250)
+        # self.resize(250, 250)
 
     def set_fields(self):
         """Get user input from the text fields then exit dialog."""
-        global addr
-        global port
-        addr = self.addressField.text()
-        port = int(self.portField.text())
+        global ADDR
+        global PORT
+        ADDR = self.addressField.text()
+        PORT = int(self.portField.text())
         self.close()
 
 
@@ -91,12 +91,14 @@ class ChatWindow(QtWidgets.QDialog):
 
     def quit(self):
         """Exit the program."""
-        tcp_client.close()
+        global TCP_CLIENT
+        TCP_CLIENT.close()
         self.close()
         kill_proc_tree(os.getpid())
 
     def send(self):
         """Send text to the lair server."""
+        global EXIT_FLAG
         text = self.chatTextField.text()
         font = self.chat.font()
         self.chat.setFont(font)
@@ -106,15 +108,21 @@ class ChatWindow(QtWidgets.QDialog):
             return self.help()
         elif text == '{quit}':
             self.chatTextField.setText('')
-            tcp_client.send(bytes('{quit}', 'utf8'))
-            self.quit()
+            try:
+                TCP_CLIENT.send(bytes('{quit}', 'utf8'))
+            except socket.error as err:
+                QtWidgets.QMessageBox.critical(self.window,
+                                               'Error',
+                                               str(err),
+                                               QtWidgets.QMessageBox.Ok)
+            EXIT_FLAG = True
 
-        # Check if client thread set the exit_flag
-        if exit_flag:
+        # Check if EXIT_FLAG is set
+        if EXIT_FLAG:
             self.quit()
         else:
             try:
-                tcp_client.send(bytes(text, 'utf8'))
+                TCP_CLIENT.send(bytes(text, 'utf8'))
                 self.chat.append(text)
                 self.chatTextField.setText('')
             except socket.error as err:
@@ -123,8 +131,6 @@ class ChatWindow(QtWidgets.QDialog):
                                                str(err),
                                                QtWidgets.QMessageBox.Ok)
                 self.quit()
-                tcp_client.close()
-                kill_proc_tree(os.getpid())
 
     def help(self):
         """Print a list of available commands."""
@@ -143,42 +149,62 @@ class ClientThread(Thread):
 
     def quit(self):
         """Exit the program."""
-        tcp_client.close()
+        global TCP_CLIENT
+        TCP_CLIENT.close()
         self.window.close()
         kill_proc_tree(os.getpid())
 
+    def recv_loop(self):
+        """Read data from server."""
+        BUFSIZ = 4096
+        global TCP_CLIENT
+        global EXIT_FLAG
+
+        while not EXIT_FLAG:
+            try:
+                data = TCP_CLIENT.recv(BUFSIZ).decode('utf8')
+            except socket.error as err:
+                QtWidgets.QMessageBox.critical(self.window,
+                                               'Error',
+                                               str(err),
+                                               QtWidgets.QMessageBox.Ok)
+                EXIT_FLAG = True
+
+            # The server closed, set EXIT_FLAG
+            if data == 'The lair is closed.':
+                EXIT_FLAG = True
+
+            # add recieved text to chat field
+            self.window.chat.append(data)
+
+        self.quit()
+
     def run(self):
         """Run the client thread."""
-        BUFSIZ = 4096
+        global ADDR
+        global PORT
+        global TCP_CLIENT
+        global EXIT_FLAG
 
-        global addr
-        global port
-        global tcp_client
         try:
-            tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            tcp_client.connect((addr, port))
+            TCP_CLIENT = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         except socket.error as err:
             QtWidgets.QMessageBox.critical(self.window,
                                            'Error',
                                            str(err),
                                            QtWidgets.QMessageBox.Ok)
-            self.quit()
+            EXIT_FLAG = True
+        try:
+            TCP_CLIENT.connect((ADDR, PORT))
+        except socket.error as err:
+            QtWidgets.QMessageBox.critical(self.window,
+                                           'Error',
+                                           str(err),
+                                           QtWidgets.QMessageBox.Ok)
+            EXIT_FLAG = True
 
-        global exit_flag
-        while not exit_flag:
-            try:
-                data = tcp_client.recv(BUFSIZ).decode('utf8')
-                if data == 'The lair is closed.':
-                    exit_flag = False
-                self.window.chat.append(data)
-            except socket.error as err:
-                exit_flag = True
-                QtWidgets.QMessageBox.critical(self.window,
-                                               'Error',
-                                               str(err),
-                                               QtWidgets.QMessageBox.Ok)
-
-        self.quit()
+        # recieve loop
+        self.recv_loop()
 
 
 # __main__? Program entry point
